f813826f1b6548eeae7a72de070e1641c332e1bc
sjiang17 <styjiang@163.com>
2018-05-15 13:08:32 -0700

    added caffe style img reader

---
+++ caffe_image_reader.py
@@ -0,0 +1,130 @@
+import torch.utils.data as data
+
+from PIL import Image
+import os
+import os.path
+from scipy.misc import imread
+
+
+IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
+
+
+def is_image_file(filename):
+    """Checks if a file is an image.
+    Args:
+        filename (string): path to a file
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    filename_lower = filename.lower()
+    return any(filename_lower.endswith(ext) for ext in IMG_EXTENSIONS)
+
+def make_dataset(dir):
+    images = []
+    dir = os.path.expanduser(dir)
+    # if not os.path.isdir(dir):
+    #     continue
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in sorted(fnames):
+            if is_image_file(fname):
+                fbasename = fname.split('.jpg')[0]
+                path = os.path.join(root, fname)
+                item = (path, fbasename)
+                images.append(item)
+
+    return images
+
+def image_loader(path):
+    im = imread(path)
+
+    if len(im.shape) == 2:
+      im = im[:,:,np.newaxis]
+      im = np.concatenate((im,im,im), axis=2)
+    # flip the channel, since the original one using cv2
+    # rgb -> bgr
+    im = im[:,:,::-1]
+    im = im.astype(np.float32, copy=False)
+    pixel_means = np.array([[[102.9801, 115.9465, 122.7717]]])
+    im -= pixel_means
+    # im = im[:, :, ::-1]
+    im_shape = im.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+    target_size = 600
+    im_scale = float(target_size) / float(im_size_min)
+    # Prevent the biggest axis from being more than MAX_SIZE
+    # if np.round(im_scale * im_size_max) > max_size:
+    #     im_scale = float(max_size) / float(im_size_max)
+    # im = imresize(im, im_scale)
+    im = cv2.resize(im, None, None, fx=im_scale, fy=im_scale,
+                    interpolation=cv2.INTER_LINEAR)
+    return im
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+# def accimage_loader(path):
+#     import accimage
+#     try:
+#         return accimage.Image(path)
+#     except IOError:
+#         # Potentially a decoding problem, fall back to PIL.Image
+#         return pil_loader(path)
+
+
+# def default_loader(path):
+#     from torchvision import get_image_backend
+#     if get_image_backend() == 'accimage':
+#         return accimage_loader(path)
+#     else:
+#         return pil_loader(path)
+
+
+class ImageFolder(data.Dataset):
+    def __init__(self, root, transform=None, target_transform=None,
+                 loader=image_loader):
+        imgs = make_dataset(root)
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+
+        self.root = root
+        self.imgs = imgs
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        # self.transform = transform
+        # self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        path, fbasename = self.imgs[index]
+        img = self.loader(path)
+        # if self.transform is not None:
+        #     img = self.transform(img)
+        # if self.target_transform is not None:
+        #     target = self.target_transform(target)
+
+        return img, fbasename
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        tmp = '    Target Transforms (if any): '
+        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
